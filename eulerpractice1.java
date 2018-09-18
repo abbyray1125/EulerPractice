9d74231fd444d46e34088d9ecbb966e69328c876
abby ray <abby.ray19@paceacademy.org>
2018-09-18 08:52:08 -0400

    initialized repository

---
+++ eulerpractice1.java
@@ -0,0 +1,29 @@
+public class eulerpractice1{
+	public static void main(String args[]){
+
+
+
+
+
+
+
+
+
+	}
+
+	public static int threeand5(int num){
+		int toret=0;
+
+
+
+
+	}
+	
+
+
+
+
+
+
+
+}
