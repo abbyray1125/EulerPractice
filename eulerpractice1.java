80a2414dd2d5a0c61bd5c434ba97e95b59bda4e5
abby ray <abby.ray19@paceacademy.org>
2018-09-18 08:55:51 -0400

    first draft

--- eulerpractice1.java
+++ eulerpractice1.java
@@ -13,10 +13,13 @@ public class eulerpractice1{
 
 	public static int threeand5(int num){
 		int toret=0;
+		for (int i=0;i<=num;i++){
+			if (i%3==0||i%5==0){
+				toret+=i;
+			}
 
-
-
-
+		}
+		return toret;
 	}
 	
 
