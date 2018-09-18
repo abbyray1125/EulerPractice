8ede91c340edca7a1b0d7a5d23d8484eba5b2641
abby ray <abby.ray19@paceacademy.org>
2018-09-18 08:58:01 -0400

    not equals

--- eulerpractice1.java
+++ eulerpractice1.java
@@ -11,9 +11,10 @@ public class eulerpractice1{
 
 	}
 
+// takes in a number and return the sum of all the multiples of 3 or 5 below it 
 	public static int threeand5(int num){
 		int toret=0;
-		for (int i=0;i<=num;i++){
+		for (int i=0;i<num;i++){
 			if (i%3==0||i%5==0){
 				toret+=i;
 			}
