2ace91e45a91bdb03b5263ceb85583a25dd8694a
abby ray <abby.ray19@paceacademy.org>
2018-09-20 10:12:17 -0400

    try 1

--- eulerpractice3.java
+++ eulerpractice3.java
@@ -4,13 +4,31 @@ public class eulerpractice3{
 
 	
 		
-		System.out.println(String.format("%,d",______));
+		System.out.println(String.format("%,d",largepfact(13195)));
 
-
+		//600,851,475,143
 
 	}
 
-	
+	// find largest prime factor
+	public static double largepfact(double num){
+		ArrayList<Double> factors=new ArrayList<Double>();
+		for (double i=2;i<Math.sqrt(num);i++){
+			if(num%i==0){
+				factors.add(i);
+				largepfact(num/i);
+			}
+		}
+		double max=2;
+		for(int i=0;i<factors.size();i++){
+			if(factors.get(i)>max){
+				max=factors.get(i);
+			}
+		}
+		return max;
+
+
+	}
 
 
 }
