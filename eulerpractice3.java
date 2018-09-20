1a2508d4ac204df9b017d76ae685e37df91974a3
abby ray <abby.ray19@paceacademy.org>
2018-09-20 09:59:45 -0400

    intialized

---
+++ eulerpractice3.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class eulerpractice3{
+	public static void main(String args[]){
+
+	
+		
+		System.out.println(String.format("%,d",______));
+
+
+
+	}
+
+	
+
+
+}
