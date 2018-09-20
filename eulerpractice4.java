f0ce344e0d8d4dbb96cc2cfad0cbe68f4910bae8
abby ray <abby.ray19@paceacademy.org>
2018-09-20 13:15:56 -0400

    done

--- eulerpractice4.java
+++ eulerpractice4.java
@@ -4,42 +4,34 @@ public class eulerpractice4{
 
 	
 		System.out.println(largepal());
+		
 
 
 	}
 
 	//find largest palindrome number made form product of 3 digit numbers
 	public static int largepal(){
-		ArrayList<Integer> pals=new ArrayList<Integer> ();
-		for (int i=100;i>=100&&i<=999;i++){
-			for (int j=100;j>=100&&j<=999;i++){
+		int curpal=0;
+		for (int i=100;i<1000;i++){
+			for (int j=100;j<1000;j++){
 				int ij=i*j;
 				String ijstring=ij+"";
-				if (ijstring.length()%2==0){
-					String first=ijstring.substring(0,ijstring.length()/2);
-					String sec=ijstring.substring(ijstring.length()/2);
-					if (first.equals(sec)){
-						pals.add(ij);
-					}
-				}
-				else{
-					String first=ijstring.substring(0,ijstring.length()/2);
-					String sec=ijstring.substring(ijstring.length()/2+1);
-					if (first.equals(sec)){
-						pals.add(ij);
-					}
+				if(checkPalindrome(ijstring)==true&&ij>curpal){
+					curpal=ij;
 				}
 			}	
 		}
-		int max=0;
-		for (int i=0;i<pals.size();i++){
-			if (pals.get(i)>max){
-				max=pals.get(i);
-			}
-		}
-		return max;
+		
+		return curpal;
 	}
 
-	
+	public static boolean checkPalindrome(String s){ 
+    	boolean toret=false;
+    	String reverse = new StringBuffer(s).reverse().toString(); 
+    	if (s.equals(reverse)){
+    		toret=true;
+    	}
+    	return toret;
+     } 
 
 }
