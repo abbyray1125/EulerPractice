55e6e5801044e485abc495d1eb1e5f998a0e78ab
abby ray <abby.ray19@paceacademy.org>
2018-09-20 13:40:17 -0400

    done

--- eulerpractice5.java
+++ eulerpractice5.java
@@ -2,13 +2,25 @@ import java.util.ArrayList;
 public class eulerpractice5{
 	public static void main(String args[]){
 
-	
+	System.out.println(isdivisible(1,20));
 		
 		
 
 
 	}
-
-	
-
+	public static int isdivisible(int lower, int upper){
+		for (int x=2520;x>0;x++){
+			for (int i=lower;i<=upper;i++){
+				if (x%i!=0){
+					break;
+				}
+				else if (x%i==0&&i==upper){
+					return x;
+				}
+			}
+		}
+		return 0;
+			
+	}
 }
+
