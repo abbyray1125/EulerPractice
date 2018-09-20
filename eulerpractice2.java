50f2cd55cdf01baceddb4d518cb00cd964489407
abby ray <abby.ray19@paceacademy.org>
2018-09-20 09:53:13 -0400

    done

---
+++ eulerpractice2.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+public class eulerpractice2{
+	public static void main(String args[]){
+
+	
+		ArrayList<Integer> list=new ArrayList<Integer> ();
+		list.add(1);
+		list.add(2);
+		System.out.println(list);
+		System.out.println(getfibb(4000000,list));
+
+
+
+	}
+
+	// takes in a upper bound and a toret and adds even fibbanaccci numbers below it
+/*
+	public static int addevenfibb(int num){
+		int toret=0;
+
+		for (int i=1;i<num;i=i){
+			if (___ %2==0){
+				//toret+=____
+			}
+		}
+}
+*/
+
+
+	
+// makes arraylist of fibbannacci numbers 
+	public static int getfibb(int upperbound, ArrayList<Integer> fibbnums){
+		int toret=2;
+		for(int i=2;fibbnums.get(i-2)+fibbnums.get(i-1)<upperbound;i++){
+			int first=fibbnums.get(i-2);
+			int sec= fibbnums.get(i-1);
+			fibbnums.add(first+sec);
+			if((first+sec)%2==0){
+				toret+=first+sec;
+			}
+		}
+		return toret;
+	}
+
+	
+
+}
