43d9754c6bf3ffb8bbc3cdaa399855ab425f93eb
abby ray <abby.ray19@paceacademy.org>
2018-09-21 13:25:07 -0400

    done

--- eulerpractice7.java
+++ eulerpractice7.java
@@ -13,10 +13,10 @@ public class eulerpractice7{
 // tells you teh indexth prime 
 	public static int numprime(int index){
 		int count=0;
-		int i=2;
+		int i=1;
 		while (count<index){
 			i++;
-			if (isprime(i)==true){
+			if (isPrime(i)==true){
 				count++;
 			}
 		}
@@ -24,7 +24,7 @@ public class eulerpractice7{
 	}
 
 //checks if number is prime
-	public static boolean isprime(int x){
+	/*public static boolean isprime(int x){
 		boolean isprime=true;
 		for (int i=2;i<x;i++){
 			if (x%i==0){
@@ -32,7 +32,16 @@ public class eulerpractice7{
 			}
 		}	
 		return isprime;
-	}
+	}*/
+	 public static boolean isPrime(int n) {
+        if (n < 2) return false;
+        else if (n == 2) return true;
+        for (int i = 2; i < Math.pow(n, 0.5) + 1; i++)
+            if (n % i == 0)
+                return false;
+        return true;
+    }
+
 
 
 }
